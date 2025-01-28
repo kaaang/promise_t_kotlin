2d795b29b5bcc853cbcace68f99d2b7818b7ca26
kaaang <kaaang@kakao.com>
2025-01-28 18:05:01 +0900

    Initial commit

---
+++ src/main/kotlin/kr/co/promise_t_kotlin/PromiseTKotlinApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.promise_t_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PromiseTKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<PromiseTKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/promise_t_kotlin/PromiseTKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.promise_t_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PromiseTKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
